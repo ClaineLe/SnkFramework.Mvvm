3a3e159c98e55aa2ba8767d8982a3fa3ec0135b4
ClaineLe <7941324@qq.com>
2022-09-02 16:44:04 +0800

    [binding]Snk.Binding-Proxy（ForUnity）迁移之Snk.Mvvm中实现

--- Runtime/Base/SnkMvvmSetup.cs
+++ Runtime/Base/SnkMvvmSetup.cs
@@ -1,9 +1,18 @@
+using Loxodon.Framework.Binding.Proxy.Targets;
 using SnkFramework.FluentBinding.Base;
 
 namespace SnkFramework.Mvvm.Base
 {
     public abstract class SnkMvvmSetup : SnkBindingSetup
     {
-    
+        protected override TargetProxyFactory CreateTargetProxyFactory()
+        {
+            TargetProxyFactory targetFactory = base.CreateTargetProxyFactory();
+            targetFactory.Register(new UnityTargetProxyFactory(), 10);
+#if UNITY_2019_1_OR_NEWER
+            targetFactory.Register(new VisualElementProxyFactory(), 30);
+#endif
+            return targetFactory;
+        }
     }
 }
